a0b7f521c182a79e8dcb2b2f58c3c3671cfba8c6
Linetgithub <awinoojwang22@gmail.com>
2023-07-14 11:48:58 -0700

    simple_shell

--- interpreter.c
+++ interpreter.c
@@ -1,89 +1,80 @@
 #include "shell.h"
-
 /**
- * _myexit - used to exit the shell
- * @info:contains arguments
- * Return: 0 exit if info.argv[0] !=
- */
-
+* _myexit - used to exit the shell
+* @info:contains arguments
+* Return: 0 exit if info.argv[0] !=
+*/
 int _myexit(info_t *info)
-{       
-        int exit;
-        
-        if (info->argv[1])/* an exit argument */
-        {       
-                exit = errbcode(info->argv[1]);
-                if (exit == -1)
-                {       
-                        info->status = 2;
-                        print_error(info, "unwanted number:");
-                        _eputs(info->argv[1]);
-                        _eputchar('\n');
-                        return (1);
-                }       
-                info->err_num = _errbcode(info->argv[1]);
-                return (-2);
-        }       
-        info->err_num = -1;
-        return (-2);
-}       
-
+{
+int exit;
+if (info->argv[1])/* an exit argument */
+{
+exit = errbcode(info->argv[1]);
+if (exit == -1)
+{
+info->status = 2;
+print_error(info, "unwanted number:");
+_eputs(info->argv[1]);
+_eputchar('\n');
+return (1);
+}
+info->err_num = _errbcode(info->argv[1]);
+return (-2);
+}
+info->err_num = -1;
+return (-2);
+}
 /**
- * _mycd - current directory has changed
- * @info: maintains a constant function prototype
- * Return:0
- */
-
+* _mycd - current directory has changed
+* @info: maintains a constant function prototype
+* Return:0
+*/
 int _mycd(info_t *info)
 {
-	char *s,*dir, buffer[1024];
-	int chdir_ret;
-
-	s = getcwd(buffer, 1024);
-	if (!s)
-		_puts("TODO:>>getcwd failure emsg here<<\n");
-	if (!info->argv[1])
-	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-			chdir_ret = /*TODO:what is it suppose to be? */
-				chdir((dir = _getenv(info,"PWD=")) ? dir : "/");
-		else
-			chdir _ret = chdir(dir);
-	}
-	else if (_strcmp(info->[1], "-") == 0)
-	{
-		if (! _getenv(info, "OLDPWD="))
-		{
-			_puts(s);
-			_putchar('\n');
-			return (1);
-		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir _ret = /* TODO: what is it suppose to be? */
-			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/")
-	}
-	else
-	{
-		 _setenv(info, "OLDPWD", _getenv(info, "PWD="));
-                _setenv(info, "PWD", getcwd(buffer, 1024));
-        }
-        return (0);
+char *s, *dir, buffer[1024];
+int chdir_ret;
+s = getcwd(buffer, 1024);
+if (!s)
+_puts("TODO:>>getcwd failure emsg here<<\n");
+if (!info->argv[1])
+{
+dir = _getenv(info, "HOME=");
+if (!dir)
+chdir_ret = /*TODO:what is it suppose to be? */
+chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+else
+chdir _ret = chdir(dir);
 }
-
+else if (_strcmp(info->[1], "-") == 0)
+{
+if (!_getenv(info, "OLDPWD="))
+{
+_puts(s);
+_putchar('\n');
+return (1);
+}
+_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+chdir _ret = /* TODO: what is it suppose to be? */
+chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/")
+}
+else
+{
+_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+_setenv(info, "PWD", getcwd(buffer, 1024));
+}
+return (0);
+}
 /**
- * _myhelp - changes the the current directory
- * @info: maintains a constant function prototype
- * Return: 0
- */
-
+* _myhelp - changes the the current directory
+* @info: maintains a constant function prototype
+* Return: 0
+*/
 int _myhelp(info_t *info)
 {
-        char **arg_array;
-        arg_array = info->argv;
-        _puts("function not implemented\n");
-        if (0)
-                _puts(*arg_array); /*temp att_unused work */
-	return (0);
+char **arg_array;
+arg_array = info->argv;
+_puts("function not implemented\n");
+if (0)
+_puts(*arg_array); /*temp att_unused work */
+return (0);
 }
-
